0565a3cb6b0ad596934cd91f6eb73082bef9486b
James Jacobs <jamesjacobs1@mac.com>
2021-03-01 10:00:17 +0000

    Updated to work reannounce when same text added to live region. Swapped to relevant roles. ES6 related changes.

--- src/LiveRegion.js
+++ src/LiveRegion.js
@@ -1,16 +1,8 @@
-/**
- * Pulsar Live Region Service
- */
-
-'use strict';
-
-import $ from 'jquery';
-
-class LiveRegion {
-
+export default class LiveRegion {
     /**
      * @constructor
      * @param {jQuery} $html jQuery wrapper of the html node
+     *
      */
     constructor ($html) {
         this.$html = $html;
@@ -20,57 +12,71 @@ class LiveRegion {
 
     /**
      * Initalise the Live Region component
-     * 
+     *
      * A polite container and an assertive container must be present in the DOM on load.
-     * 
+     * These elements must use the `role` and `aria-live` attributes seen below.
+     *
      * Example HTML:
-     *  <div role="region" class="js-live-region-polite" aria-live="polite" style="display: none;"></div>
-     *  <div role="region" class="js-live-region-assertive" aria-live="assertive" style="display: none;"></div>
-     * 
-     * Example usage: 
+     *  <div role="status" class="hide js-live-region-polite" aria-live="polite"></div>
+     *  <div role="alert" class="hide js-live-region-assertive" aria-live="assertive"></div>
+     *
+     * Note - these elements should be hidden in a screen reader friendly manner.
+     * Classes or inline style with display: none should not be used to avoid breaking
+     * live regions in certain screen readers
+     *
+     * @example
      *  liveRegion.init({
-     *      assertiveContainer: '.js-aria-live-assertive',
-     *      politeContainer: '.js-aria-live-polite'
+     *      assertiveContainer: '.js-live-region-assertive',
+     *      politeContainer: '.js-live-region-polite'
      * })
+     * @param {Object} options
      */
     init (options = {}) {
-        let _self = this;
-
-        if (typeof _self.$html === 'undefined' || !_self.$html.length) {
+        if (typeof this.$html === 'undefined' || !this.$html.length) {
             throw new Error('$html must be passed to LiveRegion');
         }
 
-        if (typeof options.assertiveContainer === undefined) {
+        if (typeof options.assertiveContainer === 'undefined') {
             throw new Error('LiveRegion must be supplied with an assertive container');
         } else {
-            _self.assertiveContainer = options.assertiveContainer;
+            this.assertiveContainer = options.assertiveContainer;
         }
 
-        if (typeof options.politeContainer === undefined) {
+        if (typeof options.politeContainer === 'undefined') {
             throw new Error('LiveRegion must be supplied wtih a polite container');
         } else {
-            _self.politeContainer = options.politeContainer;
+            this.politeContainer = options.politeContainer;
         }
     }
 
     /**
      * Populate the assertive container with a message
-     * 
-     * @param {string} content 
+     *
+     * @param {string} content
      */
     assertive (content) {
-        this.$html.find(this.assertiveContainer).text(content);
+        const $assertiveContainer = this.$html.find(this.assertiveContainer);
+        $assertiveContainer.text(content);
+
+        // Clear the text shortly after updating to make screen readers re-announce
+        // if the same text is added again
+        // Note: 150 seems to be the sweet spot, any lower and it doesn't work in VO
+        setTimeout(() => {
+            $assertiveContainer.text('');
+        }, 150);
     }
 
     /**
      * Populate the polite container with a message
-     * 
-     * @param {string} content 
+     *
+     * @param {string} content
      */
     polite (content) {
-        this.$html.find(this.politeContainer).text(content);
+        const $politeContainer = this.$html.find(this.politeContainer);
+        $politeContainer.text(content);
+
+        setTimeout(() => {
+            $politeContainer.text('');
+        }, 150);
     }
-
 }
-
-module.exports = LiveRegion;
