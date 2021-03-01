9540d9c4494b9e6c89b510f359bcec1ecf326071
James Jacobs <jamesjacobs1@mac.com>
2021-03-01 09:55:05 +0000

    Updated deps for sinon clock usage, fixed jshint

--- test/harness.js
+++ test/harness.js
@@ -1,8 +1,12 @@
 'use strict';
 
 var chai = require('chai'),
+    sinon = require('sinon'),
     sinonChai = require('sinon-chai');
 
 global.expect = chai.expect;
 
 chai.use(sinonChai);
+
+// Expose tools in the global scope
+global.sinon = sinon;
